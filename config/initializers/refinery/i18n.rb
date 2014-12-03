effc324f7acf76374420f8c4d5d8617903fbd5d9
Thai Nguyen Quang <thainq@ithanoi.com>
2014-12-03 17:05:14 +0700

    add project

---
+++ config/initializers/refinery/i18n.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+Refinery::I18n.configure do |config|
+  config.default_locale = :en
+
+  config.current_locale = :en
+
+  config.default_frontend_locale = :en
+
+  config.frontend_locales = [:en]
+
+  config.locales = {:en=>"English", :vi=>"Vietnamese"}
+end
